baa7e64e1dfff8ca1cc3601ad26a58c450305d6f
Karl <Karl@Karl-PC>
2017-12-05 13:01:49 -0800

    update wishlist caculation service

---
+++ target/generated-sources/service/com/sample/wishlistDemo/api/generated/WishlistCaculation.java
@@ -0,0 +1,32 @@
+package com.sample.wishlistDemo.api.generated;
+
+import javax.xml.bind.annotation.XmlRootElement;
+
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
+import com.fasterxml.jackson.annotation.JsonAutoDetect.Visibility;
+/**
+ * Generated dto.
+ */
+@javax.annotation.Generated(value = "hybris", date = "Sun Dec 03 06:26:48 EST 2017")
+@XmlRootElement
+@JsonAutoDetect(isGetterVisibility = Visibility.NONE, getterVisibility = Visibility.NONE, setterVisibility = Visibility.NONE,
+		creatorVisibility = Visibility.NONE, fieldVisibility = Visibility.NONE)
+public class WishlistCaculation
+{
+
+	@com.fasterxml.jackson.annotation.JsonProperty(value="wishlistid")
+	@javax.validation.constraints.NotNull
+	private java.lang.String _wishlistid;
+
+	
+
+	public java.lang.String get_wishlistid() {
+		return _wishlistid;
+	}
+
+
+
+	public void set_wishlistid(java.lang.String _wishlistid) {
+		this._wishlistid = _wishlistid;
+	}	
+}
